e7703d5c4044fb84ba29fb1c50a4b5bf908d3995
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-08-06 12:40:09 +0530

    feat : created UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import { Navbar } from './components/Navbar/Navbar'
+import {BsPencilSquare} from "react-icons/bs"
 
 function App() {
+  // const [name,setName]=useState("")
+  // const [number,setNumber]=useState("")
+
+  const [data,setData]=useState({
+    name:"",
+    number:""
+  })
+  const {name,number}=data
+  const [addContact,setAddContact]=useState([])
+  
+  const nameHandler=(e)=>{
+    setData({...data,name:e.target.value})
+  }
+  const numberHandler=(e)=>{
+    setData({...data,number:e.target.value})
+  }
+  const addContactHandler=()=>{
+    setAddContact([...addContact,data]) 
+    setData({...data, name:"",number:""})
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container">
+        <div className="contact_container">
+          <h1 className="heading_one">Contacts</h1>
+          <div className="input_container">
+            <input type="text" className="input_box" placeholder="Enter Name" value={name} onChange={nameHandler}/>
+
+            <input type="number" className="input_box" placeholder="Enter phone number" value={number} onChange={numberHandler} />
+
+            <button className="add_contact_btn" value={addContact} onClick={addContactHandler}>
+              Add<span>Contact</span>
+            </button>
+          </div>
+        </div>
+        <div className="contact_list_outer">
+          <button className="see_contect_btn">See Contacts</button>
+          {/* search */}
+          <div class="search-container">
+            <input className='search_input' placeholder="Search.." name="search" />
+            <button>
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+          {/* show item */}
+          
+          {addContact.map(({name,number})=>{
+            return (
+              <div className='item_div'>
+              <p className='paragraph'>{name}<span>{number}</span></p>
+              <div>
+                <button> <BsPencilSquare /></button>
+              </div>
+                </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+// import {AiFillGithub} from "react-icons/ai"
+
+const Navbar = () => {
+    return (
+      <div className="header">
+        {/* <Link to="/" className="logo_text"> */}
+          <h1 className="header_text_color">Royalty</h1>
+        {/* </Link> */}
+        {/* <a  href="https://github.com/neetukumari4858/address-book-manager" target= "_blank" className="header_text_color"><AiFillGithub/></a> */}
+        {/* </Link> */}
+      </div>
+    );
+  };
+  export { Navbar };
+  
